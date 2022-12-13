a894365258f2d08265a828596ed88a9ae6d7c8c1
sudenur yıldırım <119759194+sudenuryildirim@users.noreply.github.com>
2022-12-13 10:09:39 +0300

    sudenur yıldırım
    @themanoftalent

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,15 @@
 // your name surname and age from user
+#include<stdio.h>
+int main() { 
+
+   char name [10]= " SUDENUR";
+   char surname [10]="YILDIRIM";
+   int age;
+
+   printf ( " name : %s \n",name );
+   printf("surname: yıldırım: %s\n",surname);
+   printf ( " enter your age : ");
+   scanf ( " % d",&age); 
+   
+	return 0;
+}
